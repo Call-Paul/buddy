e9f6baff0bd2fb7495d20ef4340046d8663aaa4b
Paul Franken <paul.franken@otto.de>
2022-05-12 23:33:25 +0200

    Add Google Authentification and set up firebase as backend. SignIn works and sends to the home screen

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -1,21 +1,41 @@
+import 'package:buddy/services/auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SignIn extends StatefulWidget{
+class SignIn extends StatefulWidget {
   @override
   _SignInState createState() => _SignInState();
-
 }
 
 class _SignInState extends State<SignIn> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Buddy App')),
-      body: Container(
-        child: const Text('HALLO Welt'),
-      )
-    );
+        appBar: AppBar(title: Text('Buddy App')),
+        body: Center(
+            child: GestureDetector(
+              onTap: (){
+                AuthMethods().signInWithGoogle(context);
+              },
+          child: Container(
+            child: const Text(
+              'Login with Google',
+              style: TextStyle(fontSize: 30),
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(24),
+              color: Colors.amber,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          ),
+        )));
   }
-
 }
---
+++ lib/services/auth.dart
@@ -0,0 +1,57 @@
+import 'dart:developer';
+
+import 'package:buddy/helperfunctions/sharedpref_helper.dart';
+import 'package:buddy/screens/home.dart';
+import 'package:buddy/services/database.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthMethods {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  getCurrentUseres() {
+    return auth.currentUser;
+  }
+
+  signInWithGoogle(BuildContext context) async {
+    log('data: Hallo1');
+    final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+    final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+    final GoogleSignInAccount? googleSignInAccount =
+        await _googleSignIn.signIn();
+
+    if (googleSignInAccount == null) return null;
+
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
+    final AuthCredential credential = GoogleAuthProvider.credential(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken);
+    UserCredential result =
+        await _firebaseAuth.signInWithCredential(credential);
+    User? userDetails = result.user;
+
+    if (userDetails != null) {
+
+      SharedPreferencesHelper().saveUserDisplayName(userDetails.displayName!);
+      SharedPreferencesHelper().saveUserEmailKey(userDetails.email!);
+      SharedPreferencesHelper().saveUserIdKey(userDetails.uid);
+
+      Map<String, dynamic> userInfoMap = {
+        "email": userDetails.email,
+        "username": userDetails.email!.replaceAll("gmail.com", ""),
+        "name": userDetails.displayName,
+        "userid": userDetails.uid
+      };
+
+
+
+      DataBaseMethods().addUserInfoToDB(userDetails.uid, userInfoMap).then(
+          (value) => Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => Home())));
+    }
+  }
+}
